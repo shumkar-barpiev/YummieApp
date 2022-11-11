1cb9a0e8eb94db42eba664433be491fa7807ce89
Shumkar-Barpiev <barpievsh@gmail.com>
2022-11-11 15:09:06 +0600

    implement onboarding collection view

--- Views/Onboarding/OnboardingViewController.swift
+++ Views/Onboarding/OnboardingViewController.swift
@@ -10,14 +10,32 @@ import UIKit
 class OnboardingViewController: UIViewController {
     
     var slides: [OnboardingSlide] = []
+    var currentPage = 0{
+        didSet{
+            if currentPage == slides.count - 1{
+                nextButton.setTitle("Get Started", for: .normal)
+            }else{
+                nextButton.setTitle("Next", for: .normal)
+            }
+        }
+    }
 
     @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        collectionView.isPagingEnabled = true
         collectionView.delegate = self
         collectionView.dataSource = self
+        
+        slides = [
+            OnboardingSlide(title: "Delicious dishes", description: "Experience a variety of amazing dishes from different cultures around the world.", image: UIImage(named: "slide2")!),
+            OnboardingSlide(title: "World-Class Chefs", description: "Our dishes are prepared by only the best.", image: UIImage(named: "slide1")!),
+            OnboardingSlide(title: "Instant World-Wide delivery", description: "Your orders will be delivered instantly irrespective of your location around the world.", image: UIImage(named: "slide3")! )
+        ]
     }
     
     @IBAction func nextButtonClicked(_ sender: Any) {
@@ -26,7 +44,7 @@ class OnboardingViewController: UIViewController {
 }
 
 
-extension OnboardingViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+extension OnboardingViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UIScrollViewDelegate{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return slides.count
     }
@@ -37,5 +55,14 @@ extension OnboardingViewController: UICollectionViewDelegate, UICollectionViewDa
         return cell
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+    }
+    
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        currentPage = Int(scrollView.contentOffset.x / scrollView.frame.width)
+        pageControl.currentPage = currentPage
+    }
     
 }
